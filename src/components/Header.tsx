56cc45a4995a23149d0568c213c75e821db48508
leonardopf26 <leonardopf26@gmail.com>
2021-04-28 07:32:21 -0300

    Added avatar, notification and user buttons to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
 
-import { RiSearchLine } from 'react-icons/ri'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
   return (
@@ -44,6 +44,36 @@ export function Header() {
         />
 
         <Icon as={RiSearchLine} fontSize='20' />
+      </Flex>
+
+      <Flex align='center' ml='auto'>
+        <HStack
+          spacing='8'
+          mx='8'
+          pr='8'
+          py='1'
+          color='gray.300'
+          borderRightWidth={1}
+          borderColor='gray.700'
+        >
+          <Icon as={RiNotificationLine} fontSize='20' />
+          <Icon as={RiUserAddLine} fontSize='20' />
+        </HStack>
+
+        <Flex align='center'>
+          <Box mr='4' textAlign='right'>
+            <Text>Leonardo Prado</Text>
+            <Text color='gray.300' fontSize='small'>
+              leonardopf26@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar
+            size='md'
+            name='Leonardo Fontes'
+            src='https://github.com/leonardof26.png'
+          />
+        </Flex>
       </Flex>
     </Flex>
   )
