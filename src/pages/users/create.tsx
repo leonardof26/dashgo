df3c9278f873f0ca4e3a2532e0162ec03afb259d
leonardopf26 <leonardopf26@gmail.com>
2021-04-29 19:16:02 -0300

    Added structure of user creation

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,65 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  Icon,
+  SimpleGrid,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  VStack,
+} from '@chakra-ui/react'
+import { RiAddLine, RiPencilLine } from 'react-icons/ri'
+
+import { Input } from '../../components/Form/input'
+import { Header } from '../../components/Header'
+import { Pagination } from '../../components/Pagination'
+import { Sidebar } from '../../components/Sidebar'
+
+export default function CreateUser() {
+  return (
+    <Box>
+      <Header />
+      <Flex w='100%' mx='auto' my='6' maxWidth={1480} px='6'>
+        <Sidebar />
+
+        <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
+          <Heading size='lg' fontWeigth='normal'>
+            Criar Usuário
+          </Heading>
+
+          <Divider my='6' borderColor='gray.700' />
+          <VStack spacing='8'>
+            <SimpleGrid minChildWidth='240px' spacing='8' w='100%'>
+              <Input name='name' label='Nome Completo' />
+              <Input name='email' label='E-mail' type='email' />
+            </SimpleGrid>
+            <SimpleGrid minChildWidth='240px' spacing='8' w='100%'>
+              <Input name='password' label='Senha' type='password' />
+              <Input
+                name='password_confirmation'
+                label='Confirmação da Senha'
+                type='password'
+              />
+            </SimpleGrid>
+          </VStack>
+
+          <Flex mt='8' justify='flex-end'>
+            <HStack spacing='4'>
+              <Button colorScheme='whiteAlpha'>Cancelar</Button>
+              <Button colorScheme='pink'>Salvar</Button>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
