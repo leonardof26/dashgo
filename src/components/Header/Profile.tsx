c88c7a1d9ce10efab6f8afcf358b64cc3af952de
leonardopf26 <leonardopf26@gmail.com>
2021-05-01 08:57:35 -0300

    Reetructered components layout into multi components

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,20 @@
+import { Avatar, Box, Flex, Text } from '@chakra-ui/react'
+
+export function Profile() {
+  return (
+    <Flex align='center'>
+      <Box mr='4' textAlign='right'>
+        <Text>Leonardo Prado</Text>
+        <Text color='gray.300' fontSize='small'>
+          leonardopf26@gmail.com
+        </Text>
+      </Box>
+
+      <Avatar
+        size='md'
+        name='Leonardo Fontes'
+        src='https://github.com/leonardof26.png'
+      />
+    </Flex>
+  )
+}
