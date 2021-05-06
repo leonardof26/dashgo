ccd3f91522d13b9605cca7e79e48006f53401163
leonardopf26 <leonardopf26@gmail.com>
2021-05-06 07:34:14 -0300

    Added user list and react query devtools

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -26,13 +26,37 @@ import { Header } from '../../components/Header'
 import { Pagination } from '../../components/Pagination'
 import { Sidebar } from '../../components/Sidebar'
 
-export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json()
+type User = {
+  id: string
+  name: string
+  email: string
+  createdAt: string
+}
 
-    return data
-  })
+export default function UserList() {
+  const { data, isLoading, error } = useQuery(
+    'users',
+    async () => {
+      const response = await fetch('http://localhost:3000/api/users')
+      const data = await response.json()
+
+      const users = data.users.map((user: User) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric',
+          }),
+        }
+      })
+
+      return users
+    },
+    { staleTime: 1000 * 5 }
+  )
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -84,33 +108,22 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={['4', '4', '6']}>
-                      <Checkbox colorScheme='pink' />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight='bold'>Leonardo Fontes</Text>
-                        <Text fontSize='small' color='gray.300'>
-                          leonardopf26@gmail.com
-                        </Text>
-                      </Box>
-                    </Td>
-                    {!!isWideVersion && <Td>19 maio 2021</Td>}
-                    {!!isWideVersion && (
-                      <Td>
-                        <Button
-                          as='a'
-                          size='sm'
-                          fontSize='sm'
-                          colorScheme='purple'
-                          leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
-                        >
-                          Editar
-                        </Button>
+                  {data.map((user: User) => (
+                    <Tr key={user.id}>
+                      <Td px={['4', '4', '6']}>
+                        <Checkbox colorScheme='pink' />
                       </Td>
-                    )}
-                  </Tr>
+                      <Td>
+                        <Box>
+                          <Text fontWeight='bold'>{user.name}</Text>
+                          <Text fontSize='small' color='gray.300'>
+                            {user.email}
+                          </Text>
+                        </Box>
+                      </Td>
+                      {!!isWideVersion && <Td>{user.createdAt}</Td>}
+                    </Tr>
+                  ))}
                 </Tbody>
               </Table>
               <Pagination />
